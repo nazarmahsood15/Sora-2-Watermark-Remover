9916fa4e240ef2b49bebddc22a30feab1328aa85
Nazar Muhammad Khan Mahsood <nazarmahsood16@gmail.com>
2025-10-24 23:43:59 +0500

    Your commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -162,6 +163,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+          <Script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6138812664032756"
+  crossOrigin="anonymous"
+  strategy="afterInteractive"
+/>
+
         {/* Preload critical resources */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="href=https://fonts.gstatic.com" crossOrigin="" />
@@ -220,6 +228,7 @@ export default function RootLayout({
           }}
         />
 
+
         {/* WebSite Schema */}
         <script
           type="application/ld+json"
@@ -238,6 +247,7 @@ export default function RootLayout({
           }}
         />
       </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
