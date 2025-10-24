7e6335f4f42dc0c38740c486cdfbf8ab362b2697
Nazar Muhammad Khan Mahsood <nazarmahsood16@gmail.com>
2025-10-24 23:48:53 +0500

    Your commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -163,6 +163,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+         <meta name="google-site-verification" content="YOUR_ADSENSE_CODE_HERE" />
           <Script
   async
   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6138812664032756"
